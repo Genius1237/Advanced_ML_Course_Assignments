9be0c3b5a978da00665cbc06af8a325a080bcf78
Bhk <bharatraghunthan9767@gmail.com>
2018-04-15 18:12:31 +0530

    Added preprocessing step as per requirements

--- A3/src/ann.py
+++ A3/src/ann.py
@@ -10,26 +10,28 @@ from keras import backend as K
 
 batch_size = 128
 num_classes = 10
-epochs = 60
+epochs = 30  #Validation Error increases after 30 epochs
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2])
-# y_train = y_train.reshape(y_train.shape[0], 1)
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2])
-# y_test = y_test.reshape(y_test.shape[0], 1)
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2]).astype(float)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2]).astype(float)
+
+#To make pixel value ranges between [0, 1] as asked in the assignment, also improves the performance of the ANN
+X_train/=255.0
+X_test/=255.0
+
 y_train = keras.utils.to_categorical(y_train, num_classes=num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes=num_classes)
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
 
 model = Sequential()
 model.add(Dense(100, input_dim=X_train.shape[1], activation='sigmoid'))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(optimizer='adam', loss='categorical_crossentropy')
-model.fit(x=X_train, y=y_train, batch_size=batch_size, validation_split=0.2, epochs=epochs)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=X_train, y=y_train, batch_size=batch_size, validation_split=0.2, epochs=epochs, verbose=1)
 
-model.evaluate(x=X_test, y=y_test, verbose=1)
+score = model.evaluate(x=X_test, y=y_test, verbose=1)
+#Actually display the test loss and accuracy
+print('Test Loss: ', score[0])
+print('Test Accuracy: ', score[1])
